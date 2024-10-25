75b3ff18b58779fdbeb41edce7d41da001f3e82a
Luigi De Brito Piantavinha <szochoralewizc@hotmail.com>
2024-10-25 13:52:08 +0100

    libft->ft_strjoin

--- libft_42/ft_strjoin.c
+++ libft_42/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: lude-bri <lude-bri@42student.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 11:04:52 by lude-bri          #+#    #+#             */
-/*   Updated: 2024/10/25 13:44:54 by luigi            ###   ########.fr       */
+/*   Updated: 2024/10/25 13:52:02 by luigi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,55 +25,55 @@
  * Return: A pointer to the newly allocated string containing 
  * the concatenation of 's1' and 's2', or NULL if memory allocation fails.
  */
-// char	*ft_strjoin(char const *s1, char const *s2)
-// {
-// 	size_t	i;
-// 	size_t	j;
-// 	char	*new_str;
-//
-// 	i = 0;
-// 	j = 0;
-// 	if (!s1 || !s2)
-// 		return (NULL);
-// 	new_str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-// 	if (!new_str)
-// 		return (0);
-// 	while (s1[i])
-// 	{
-// 		new_str[i] = s1[i];
-// 		i++;
-// 	}
-// 	while (s2[j])
-// 	{
-// 		new_str[i++] = s2[j];
-// 		j++;
-// 	}
-// 	new_str[i] = 0;
-// 	return (new_str);
-// }
-
 char	*ft_strjoin(char *s1, char *s2)
 {
-	int		i;
-	char	*tmp;
-	char	*new;
+	size_t	i;
+	size_t	j;
+	char	*new_str;
 
 	i = 0;
-	if (!*s1)
-		*s1 = '\0';
+	j = 0;
 	if (!s1 || !s2)
 		return (NULL);
-	tmp = s1;
-	new = malloc((ft_strlen(s1) + ft_strlen(s2)+ 1) * sizeof(char));
-	if (!new)
-		return (NULL);
-	if (s1)
-		while (*s1)
-			new[i++] = *s1++;
-	while (*s2)
-		new[i++] = *s2++;
-	new[i] = '\0';
-	if (tmp)
-		free(tmp);
-	return (new);
+	new_str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!new_str)
+		return (0);
+	while (s1[i])
+	{
+		new_str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		new_str[i++] = s2[j];
+		j++;
+	}
+	new_str[i] = 0;
+	return (new_str);
 }
+
+// char	*ft_strjoin(char *s1, char *s2)
+// {
+// 	int		i;
+// 	char	*tmp;
+// 	char	*new;
+//
+// 	i = 0;
+// 	if (!*s1)
+// 		*s1 = '\0';
+// 	if (!s1 || !s2)
+// 		return (NULL);
+// 	tmp = s1;
+// 	new = malloc((ft_strlen(s1) + ft_strlen(s2)+ 1) * sizeof(char));
+// 	if (!new)
+// 		return (NULL);
+// 	if (s1)
+// 		while (*s1)
+// 			new[i++] = *s1++;
+// 	while (*s2)
+// 		new[i++] = *s2++;
+// 	new[i] = '\0';
+// 	if (tmp)
+// 		free(tmp);
+// 	return (new);
+// }
